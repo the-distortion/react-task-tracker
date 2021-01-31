8066a102b96d1a2000626c6eafc924abb83a9345
the-distortion <soham86das@gmail.com>
2021-01-31 22:45:33 +0530

    improved logic

--- src/App.js
+++ src/App.js
@@ -8,23 +8,41 @@ function App() {
       id: 1,
       text: "one",
       day: "DD-MM-YY @ HH:MM",
+      reminder: true,
     },
     {
       id: 2,
       text: "tooo",
       day: "DD-MM-YY @ HH:MM",
+      reminder: true,
     },
     {
       id: 3,
       text: "three",
       day: "DD-MM-YY @ HH:MM",
+      reminder: true,
     },
   ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((tasks) => tasks.id !== id ));
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    // tasks.forEach((task) => {
+    //   if(task.id === id) {
+    //     document.querySelectorAll('.task.reminder')[id].style = { display: "none"};
+    //   }
+    // });
+    setTasks(tasks.map( (task) => task.id === id ? { ...task, reminder: !task.reminder } : task ));
+  }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'Nothing lmao'}
     </div>
   );
 }
